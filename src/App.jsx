89c96badf792f92944ca9f7dec3e718c192f03f8
isabellacastro02 <isabella.mcastro02@gmail.com>
2025-08-25 08:04:49 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,12 @@ function App() {
 
         {/* Produto 1 */}
         <div className="card">
+        <img src={TenisNike} alt="" className="img-prod"/>
           <h2>Tenis Nike</h2>
           <p className="desc">Um lindo tenis da nike</p>
           <p className="preco">R$699,99</p>
           <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-          <img src={TenisNike} alt="" className="img-prod"/>
+          <div className="off">-15%</div>
         </div>
 
       </section>
