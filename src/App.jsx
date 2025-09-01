e527b35d89d5b2186b3538eb58d69f7d3c6d10b8
isabellacastro02 <isabella.mcastro02@gmail.com>
2025-09-01 10:55:46 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import TenisNike from './assets/tenis-nike.webp'
+import IphoneRosa from './assets/iphonerosa.jpg'
+import IphoneVerde from './assets/iphoneverde.jpg'
+import IphoneCinza from './assets/iphonecinza.jpg'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,34 +17,34 @@ function App() {
         {/* Produto 1 */}
         <a href="#" className="link-card">
         <div className="card">
-        <img src={TenisNike} alt="" className="img-prod"/>
-          <h2>Tênis Nike</h2>
-          <p className="desc">Um lindo tenis da nike</p>
-          <p className="preco">R$699,99</p>
-          <div className="avaliacao"> <span>★ ★ ☆ ☆ ☆</span> (25)</div>
-          <div className="off">-15%</div>
+        <img src={IphoneRosa} alt="" className="img-prod"/>
+          <h2>Iphone Rosa</h2>
+          <p className="desc">Iphone 15 (128GB) Rosa</p>
+          <p className="preco">R$6.499,00</p>
+          <div className="avaliacao"> <span>★ ★ ★ ★ ☆</span> (124)</div>
+          <div className="off">-10% no pix</div>
         </div>
       </a>
         {/* Produto 2 */}
         <a href="#" className="link-card">
         <div className="card">
-        <img src={TenisNike} alt="" className="img-prod"/>
-          <h2>Tênis Nike</h2>
-          <p className="desc">Um lindo tenis da nike</p>
-          <p className="preco">R$699,99</p>
-          <div className="avaliacao"> <span>★ ★ ★ ★ ☆ </span> (50)</div>
+        <img src={IphoneVerde} alt="" className="img-prod"/>
+          <h2>Iphone Verde</h2>
+          <p className="desc">Iphone 15 (256GB) Verde</p>
+          <p className="preco">R$4.558,00</p>
+          <div className="avaliacao"> <span>★ ★ ★ ★ ★ </span> (226)</div>
           <div className="off">-15%</div>
         </div>
       </a>
         {/* Produto 3 */}
         <a href="#" className="link-card">
         <div className="card">
-        <img src={TenisNike} alt="" className="img-prod"/>
-          <h2>Tênis Nike</h2>
-          <p className="desc">Um lindo tenis da nike</p>
-          <p className="preco">R$699,99</p>
-          <div className="avaliacao"> <span>★ ★ ★ ★ ★</span> (128)</div>
-          <div className="off">-15%</div>
+        <img src={IphoneCinza} alt="" className="img-prod"/>
+          <h2>Iphone Titânio</h2>
+          <p className="desc">IPhone 16 Pro Max (1TB) Titânio Natural</p>
+          <p className="preco">R$11.998,00</p>
+          <div className="avaliacao"> <span>★ ★ ★ ☆ ☆</span> (85)</div>
+          <div className="off">-10% no pix</div>
         </div>
       </a>
 
